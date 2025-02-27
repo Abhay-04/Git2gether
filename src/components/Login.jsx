d92b3090adcc05a23bd8059b5605427275c83d7a
Abhay Sharma <abhysharma5113@gmail.com>
2025-02-27 17:40:52 +0530

    add : signup

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+  const [isLoginPage , setIsLoginPage] = useState(false)
   return (
     <div className="min-h-[75vh] mt-20">
       <div className="flex flex-col gap-3 w-[20%]  mx-auto px-6 py-8  rounded-md border">
@@ -10,13 +12,33 @@ const Login = () => {
             Enter your email below to login to your account
           </h1>
         </div>
+      { isLoginPage && <label className="form-control w-full max-w-xs">
+          <div className="label">
+            <span className="label-text font-semibold">Name</span>
+          </div>
+          <input
+            type="text"
+            placeholder="Name"
+            className="input input-bordered w-full max-w-xs"
+          />
+        </label>}
+      { isLoginPage && <label className="form-control w-full max-w-xs">
+          <div className="label">
+            <span className="label-text font-semibold">Username</span>
+          </div>
+          <input
+            type="text"
+            placeholder="Username"
+            className="input input-bordered w-full max-w-xs"
+          />
+        </label>}
 
         <label className="form-control w-full max-w-xs">
           <div className="label">
             <span className="label-text font-semibold">Email</span>
           </div>
           <input
-            type="text"
+            type="email"
             placeholder="m@example.com"
             className="input input-bordered w-full max-w-xs"
           />
@@ -24,7 +46,7 @@ const Login = () => {
         <label className="form-control w-full max-w-xs">
           <div className="label font-semibold">
             <span className="label-text">Password</span>
-            <Link className="label-text-alt">Forgot your password?</Link>
+            {!isLoginPage && <Link className="label-text-alt">Forgot your password?</Link>}
           </div>
           <input
             type="text"
@@ -32,10 +54,10 @@ const Login = () => {
             className="input input-bordered w-full max-w-xs"
           />
         </label>
-        <button className="btn btn-active btn-primary">Login</button>
-        <button className="btn btn-outline btn-primary">Login with Google</button>
-
-        <h1 className="text-center font-semibold">Don't have an account?<Link className="ml-1 underline">Sign up</Link></h1>
+        <button className="btn btn-active btn-primary">{isLoginPage ? "Signup" : "Login"}</button>
+       { !isLoginPage && <button className="btn btn-outline btn-primary">Login with Google</button>
+}
+        <h1 className="text-center font-semibold">{ isLoginPage ? "Already have an account?" :  "Don't have an account?"}<Link onClick={()=> setIsLoginPage(!isLoginPage)} className="ml-1 underline">{isLoginPage ? "Login" : "Signup"}</Link></h1>
       </div>
     </div>
   );

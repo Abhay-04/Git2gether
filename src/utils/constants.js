22c1e2fef1af864a8d5cd7737e01b9c65d34a989
Abhay Sharma <abhysharma5113@gmail.com>
2025-04-16 18:52:19 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL_PROD = "/api"
+export const BASE_URL = "/api"
 // export const BASE_URL_DEV = "http://localhost:3000"
 
 // export const BASE_URL = "https://git2gether-backend.onrender.com";

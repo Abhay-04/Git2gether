d5795ecc7949194d9fb3c75b5fcb8d8e6598a190
Abhay Sharma <abhysharma5113@gmail.com>
2025-04-21 15:07:24 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,4 +2,4 @@
 // export const BASE_URL = "http://localhost:3000"
 
 // export const BASE_URL = "/api";
-export const BASE_URL = "http://3.108.65.27/";
+export const BASE_URL = "http://3.108.65.27";
